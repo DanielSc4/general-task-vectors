0f388901a82265646e176fd66a8d4935ef15c311
daniel <scalena99@gmail.com>
2024-01-12 11:58:54 +0100

    final_test

--- src/utils/model_utils.py
+++ src/utils/model_utils.py
@@ -117,13 +117,13 @@ def load_gpt_model_and_tokenizer(
             'd_model': model.config.hidden_size,     # residual stream
             'name': model.config._name_or_path,
             'vocab_size': model.config.vocab_size,
-            'layer_name': 'model.model.layer',
+            'layer_name': 'model.layer',
             'layer_hook_names': [
-                f'model.model.layer.{layer}' for layer in range(model.config.num_hidden_layers)
+                f'model.layer.{layer}' for layer in range(model.config.num_hidden_layers)
             ],
             'attn_name': 'self_attn',
             'attn_hook_names': [
-                f'model.model.layer.{layer}.self_attn' for layer in range(model.config.num_hidden_layers)
+                f'model.layer.{layer}.self_attn' for layer in range(model.config.num_hidden_layers)
             ],
         }
 
