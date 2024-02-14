b9375329c6e3ae1141861b73a121fa17b3334675
Daniel <scalena99@gmail.com>
2024-02-14 09:23:38 +0000

    prepro. and test data

--- data/danger_data/preprocessing.py
+++ data/danger_data/preprocessing.py
@@ -16,17 +16,28 @@ def load_json_dataset(json_path):
         dataset = json.load(file)
     return dataset
 
-def main():
-    files_in_current_dir = os.listdir('.')
-    for file in fi
+def main(
+    json_path: str,
+    output_path: str = 'data/'
+):
 
-    load_json_dataset()
+    dataset = load_json_dataset(json_path)
 
+    print('Printing preview of the dataset')
+    for k in dataset.keys():
+        print(f'\tKey: {k} [0:2]')
+        print(f'\t\t {dataset[k][0:2]}')
 
+    # instructions is the only important column
+    output = [{'input': v, 'output': ''} for v in dataset['instructions']]
 
+    # write output file in output_path
+    # get dataset name from path
+    dataset_name = json_path.split('/')[-1]
+    
 
-
-
+    with open(output_path + dataset_name, 'w') as fout:
+        json.dump(output, fout, indent=4)
 
 
 if __name__ == "__main__":
