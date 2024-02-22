de6c3a5eaaf0f7c3de364e5c424db75af210625f
Daniel <scalena99@gmail.com>
2024-02-22 16:42:40 +0000

    chore: but eval not tested

--- src/utils/eval/general_evaluation_fun.py
+++ src/utils/eval/general_evaluation_fun.py
@@ -2,10 +2,11 @@ import torch
 import random
 from tqdm import tqdm
 import numpy as np
+from src.utils.eval.multi_token_evaluator import Evaluator
 
 
 from src.utils.prompt_helper import tokenize_ICL, randomize_dataset
-from src.intervention import replace_heads_w_avg, simple_forward_pass
+from src.intervention import replace_heads_w_avg, simple_forward_pass, replace_heads_w_avg_multi_token
 
 
 def calculate_accuracy(probs: dict[str, list], labels: list, top_k_accuracy: bool | int = False):
@@ -155,3 +156,92 @@ def evaluate_tv_kshot(
             print(f'    top {len(probs_edited["top_k_tokens"][idx])} tokens predicted: {[tokenizer.decode(ele) for ele in probs_edited["top_k_tokens"][idx]]}')
             print()
 
+
+
+def evaluate_tv_multi_token(
+    mean_activation: torch.Tensor,
+    top_heads: list[tuple[int, int]],
+    model,
+    tokenizer,
+    config,
+    prompts_from_dataset: list[tuple[str, str]],
+    evaluator: Evaluator,
+    print_examples = True,
+):
+    
+    all_tokenized_prompt, all_important_ids, _ = tokenize_ICL(
+        tokenizer=tokenizer,
+        ICL_examples=0,
+        dataset=prompts_from_dataset,
+    )
+
+    results = []
+    pbar = tqdm(
+        zip(all_tokenized_prompt, all_important_ids),
+        total=len(all_tokenized_prompt),
+        desc='[x] Generating responses'
+    )
+    for prompt, imp_ids in pbar:
+        original_output = simple_forward_pass(model=model, prompt=prompt, multi_token_generation=True).cpu().squeeze()
+
+        edited_output = replace_heads_w_avg_multi_token(
+            tokenized_prompt=prompt,
+            important_ids=[imp_ids],
+            layers_heads=top_heads,
+            avg_activations=[mean_activation[i, j] for i, j in top_heads],
+            model=model,
+            config=config,
+            pad_token_id=tokenizer.pad_token_id,
+        )
+
+        # evaluate both outputs
+        only_output_original = tokenizer.decode(
+            original_output.squeeze()[prompt.shape[0] :],
+            skip_special_tokens=True,
+        )
+        only_output_edited = tokenizer.decode(
+            edited_output.squeeze()[prompt.shape[0] :],
+            skip_special_tokens=True,
+        )
+
+        original_eval_res = evaluator.get_evaluation([only_output_original])
+        edited_eval_res = evaluator.get_evaluation([only_output_edited])
+
+        results.append({
+            "prompt": tokenizer.decode(prompt, skip_special_tokens=True),
+            "original_output": only_output_original,
+            "original_output_score": original_eval_res,
+            "edited_output": only_output_edited,
+            "edited_output_score": edited_eval_res,
+        })
+
+
+
+    # Print here the evaluation results
+    print('------- Evaluation results -------')
+    original_scores = [s['original_output_score'] for s in results]
+    edited_scores = [s['edited_output_score'] for s in results]
+
+    print('Original model scores:')
+    for ele in set(original_scores):
+        count = original_scores.count(ele)
+        print(f'  Label {ele}: {count} out of {len(results)}. ({count / len(results) :.2f})')
+        
+    print('Edited model scores:')
+    for ele in set(edited_scores):
+        count = edited_scores.count(ele)
+        print(f'  Label {ele}: {count} out of {len(results)}. ({count / len(results) :.2f})')
+
+
+    if print_examples:
+        # print out min(10, 10% of the dataset) examples
+        idx_to_print = random.sample(
+            range(len(all_tokenized_prompt)),
+            min(10, int(np.ceil(0.1 * len(results)))),
+        )
+
+        for idx in idx_to_print:
+            print('Prompt: ' + results[idx]['prompt'].replace('\n', ' '))
+            print(f'  Original [score: {results[idx]["original_output_score"]}]: ' + results[idx]['original_output'].replace('\n', ' '))
+            print(f'  Edited   [score: {results[idx]["edited_output_score"]}]: ' + results[idx]['edited_output'].replace('\n', ' '))
+            
