c88ab4a590963b5ab1b2b883adf67ea5a3a25793
Daniel <scalena99@gmail.com>
2024-03-25 10:32:17 +0000

    checking humor

--- run_local.sh
+++ run_local.sh
@@ -3,9 +3,9 @@
 
 (yes | python -m main \
     --model_name stabilityai/stablelm-2-zephyr-1_6b \
-    --dataset_name D-CoNa \
+    --dataset_name cona-humor \
     --multi_token_generation \
-    --mean_support 25 \
+    --mean_support 20 \
     --aie_support 20 \
     --icl_examples 4 \
     --batch_size 1 \
