284e78a560ed9eaa9d7fa67efcb12b4dd9967257
Daniel <scalena99@gmail.com>
2024-03-27 14:06:42 +0000

    eval improvements

--- run_debug.sh
+++ run_debug.sh
@@ -2,25 +2,25 @@
 # source .venv/bin/activate
 
 
-echo "Running main"
-python -m main \
+# echo "Running main"
+# python -m main \
+#     --model_name openai-community/gpt2 \
+#     --dataset_name XS_test \
+#     --multi_token_generation \
+#     --mean_support 4 \
+#     --aie_support 3 \
+#     --icl_examples 4 \
+#     --batch_size 1 \
+#     --load_in_8bit \
+
+echo "Running eval"
+python -m eval \
     --model_name openai-community/gpt2 \
     --dataset_name XS_test \
-    --multi_token_generation \
-    --mean_support 4 \
-    --aie_support 3 \
-    --icl_examples 4 \
-    --batch_size 1 \
+    --mean_activation_name mean_activations_icl4_sup4.pt \
+    --cie_name cie_ICL4_sup3.pt \
+    --top_n_heads 10 \
+    --eval_dim 10 \
     --load_in_8bit \
-
-# echo "Running eval"
-# python -m eval \
-#     --model_name \
-#     --dataset_name \
-#     --mean_activation_path \
-#     --cie_path \
-#     --top_n_heads \
-#     --eval_dim \
-#     --load_in_8bit \
-#     --print_examples \
+    --print_examples \
 
